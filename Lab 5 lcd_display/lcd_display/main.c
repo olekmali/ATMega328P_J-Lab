7f83e3856dec681d3168a173b7acbbdc8fb33444
olekmali <olekmali@ieee.org>
2020-10-14 00:45:48 -0500

    Renamed Projects

---
+++ Lab 5 lcd_display/lcd_display/main.c
@@ -0,0 +1,24 @@
+//* blink and toggle in c *
+#include <stdint.h>
+#include "bios_leds.h"
+#include "bios_keys.h"
+
+#define F_CPU (16000000UL)
+#include <util/delay.h>
+
+// this test allows you to do the final tweak of the delay function
+int main()
+{
+    leds_init();
+    keys_init();
+
+    while(1)
+    {
+        uint8_t bit5 = leds_get() & 0b00100000;
+        bit5 = bit5 ^ 0b00100000;
+        leds_set( keys_get() | bit5 );
+        _delay_ms(100);
+    }
+
+    return(0);
+}
