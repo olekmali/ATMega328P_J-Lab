e927b7baa264e67cd45e9f2cafb6ef80aece122e
olekmali <olekmali@ieee.org>
2020-10-25 01:42:32 -0500

    Corrected 4C example on sending timing sequences Added 4C example showing pin chnaged interrupt that starts a timer, and then when the timing sequence is done the uC goes back to power down mode

---
+++ Lab 4C pinc_n_timer/pinc_n_timer/main.c
@@ -0,0 +1,108 @@
+//* testing PINC restarting TIMER1 interrupt  - main.c *
+#include <stdint.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+#include "bios_leds.h"
+#include "bios_keys.h"
+#include "bios_timer_int.h"
+#include "bios_pinc_int.h"
+
+
+//------------------------------------------------------------------------------------
+// Global constant(s)
+//------------------------------------------------------------------------------------
+
+#define INT_FREQUENCY      1000L
+
+//------------------------------------------------------------------------------------
+// HAL functions for the interrupts
+//------------------------------------------------------------------------------------
+inline void T1intON()  { TIMSK1 = (1<<OCIE1A); }
+inline void T1intOFF() { TIMSK1 = 0; }
+inline void sleep_On() { set_sleep_mode(SLEEP_MODE_PWR_SAVE); sleep_enable(); sleep_cpu(); }
+inline void sleep_Off(){ sleep_disable(); }
+
+//------------------------------------------------------------------------------------
+// Global variable(s) used as bridge to pass parameters to the interrupts
+//------------------------------------------------------------------------------------
+static volatile uint8_t need_to_sleep = 0;
+static volatile uint8_t t1_reactivate = 0;
+
+
+//------------------------------------------------------------------------------------
+// Timer1 Interrupt Functionality
+//------------------------------------------------------------------------------------
+void MyTimerFN (void)
+{
+    static uint16_t counter = 0;
+    if ( counter == 0 ) {
+        if ( t1_reactivate!=0 ) {
+            leds_set( leds_get() | B_L0 ); // this line is for demo purposes
+            t1_reactivate = 0;
+            counter = 1024;
+        } else {
+            leds_set( leds_get() & ~B_L0 ); // this line is for demo purposes
+            T1intOFF();
+            need_to_sleep = 1; // shut down the activity
+            // DO NOT power down inside an interrupt as new interrupts are disabled
+            // and there will be no wakeup due to a triggered interrupt
+        }
+    } else {
+        counter--;
+    }
+
+}
+
+void MyInterruptPinChangedFunction () {
+    t1_reactivate = 1;
+    T1intON();
+    need_to_sleep = 0;
+    sleep_Off();
+}
+
+void setup_PC012() {
+    DDRC =  DDRC  & 0b11111000; // don't touch the upper 5 pin setup, just make sure that the lowest 3 pins are input/zero
+    PORTC = PORTC | 0b00000111; // don't touch the upper 5 pin setup, just make sure that the lowest 3 pins has pull up resistors/one
+
+    // enable the interrupt on the lowest 3 pins of PORTC
+    PCINT_PINC_initialize( 0b00000111, MyInterruptPinChangedFunction );   // enable pin change interrupt on PINC.0
+}
+
+void setup_Unused_Pins() {
+    // see example power down mode with pin changed interrupt for explanation
+    DDRC  &= 0b11000111;
+    PORTC |= 0b00111000;
+    DDRD  &= 0b00000011;
+    PORTD |= 0b11111100;
+}
+
+int main(void)
+{
+    leds_init();
+    leds_set(0);
+    keys_init();
+    Timer1_initialize( INT_FREQUENCY , MyTimerFN, timer_prescale_1 );
+    setup_PC012();
+    setup_Unused_Pins();
+
+    sei();
+    need_to_sleep = 1;
+    while(1) {
+        if ( need_to_sleep!=0 ) {
+            need_to_sleep = 0;
+            sleep_On();
+        }
+
+        {   // the code fragment below is for illustration purposes only
+            // it indicates that the main loop is running
+            cli(); leds_set( leds_get() | B_L4 ); sei();
+            for (volatile uint32_t i =0; i<30000; i++) ;
+            cli(); leds_set( leds_get() & ~B_L4 ); sei();
+            for (volatile uint32_t i =0; i<30000; i++) ;
+        }
+
+    }
+
+    return(0);
+}
--- Lab 4C timer_sequence/timer_sequence/main.c
+++ Lab 4C timer_sequence/timer_sequence/main.c
@@ -1,8 +1,8 @@
 //* testing TIMER1 interrupt with sequence generation - main.c *
-#include "bios_timer_int.h"
 #include <stdint.h>
 #include <avr/interrupt.h>
 
+#include "bios_timer_int.h"
 #include "bios_leds.h"
 #include "bios_keys.h"
 
@@ -10,98 +10,106 @@
 //------------------------------------------------------------------------------------
 // Global constant(s)
 //------------------------------------------------------------------------------------
-//#define INT_FREQUENCY   38000L
-#define INT_FREQUENCY      2500L
-#define MAIN_LOOP_FREQUENCY 100
-
-#define SEQ_CNT     2   // how many sequences
-#define SEQ_LEN    10   // maximum length of a sequence (otherwise, 0-terminated)
+//#define INT_FREQUENCY   72000L // For IR remote
 //#define SEQ_STEP 3800
-#define SEQ_STEP 250   // how many interrupts for one sequence
-#define SEQ_NONE SEQ_CNT
+
+#define INT_FREQUENCY      2000L // For 1kHz modulated Morse code signals
+#define STEP_SIZE 400       // 0.2s per 1 unit of the array
 
 
-typedef uint16_t udata_t;
 
-const udata_t ARRAY[SEQ_CNT][SEQ_LEN] = {
-        { 1, 1, 3, 4, 0, 0, 0, 0, 0, 0 },   // 'A'
-        { 3, 1, 3, 1, 3, 4, 0, 0, 0, 0 }    // 'O'
-    };
-
-inline void sendON()   { PORTB = PORTB ^ 0b00000001; }
-inline void sendOFF()  { PORTB = PORTB & 0b10000000; }
+//------------------------------------------------------------------------------------
+// HAL functions for the interrupts
+//------------------------------------------------------------------------------------
 inline void T1intON()  { TIMSK1 = (1<<OCIE1A); }
 inline void T1intOFF() { TIMSK1 = 0; }
+
+// Passive Buzzer Morse Code / IR Remote
+//inline void sendON()        { }
+//inline void sendOFF()       { PORTB = PORTB & 0b11111110; }
+//inline void sendStartONE()  { PORTB = PORTB | 0b00000001; }
+//inline void sendStartTWO()  { PORTB = PORTB & 0b11111110; }
+//inline void sendGoOnONE()   { PORTB = PORTB ^ 0b00000001; }
+//inline void sendGoOnTWO()   { PORTB = PORTB & 0b11111110; }
+
+// On-off Morse code
+inline void sendON()        { }
+inline void sendOFF()       { PORTB = PORTB & 0b11111110; }
+inline void sendStartONE()  { PORTB = PORTB | 0b00000001; }
+inline void sendStartTWO()  { PORTB = PORTB & 0b11111110; }
+inline void sendGoOnONE()   { }
+inline void sendGoOnTWO( )  { }
 
 //------------------------------------------------------------------------------------
 // Global variable(s) used as bridge to pass parameters to the interrupts
 //------------------------------------------------------------------------------------
-static volatile uint8_t data_next = SEQ_NONE;
+static const uint16_t* data_next_pointer = 0;
 
-void send( uint8_t to_send ) {
-    if ( SEQ_NONE == data_next ) {
-        data_next = to_send;
+void sendMessage( const uint16_t to_send[] ) {
+    if ( 0 == data_next_pointer ) {
+        data_next_pointer = to_send;
         T1intON();
     }
 }
+
 
 //------------------------------------------------------------------------------------
 // Timer1 Interrupt Functionality
 //------------------------------------------------------------------------------------
 void MyTimerFN (void)
 {
-    static udata_t loop_counter;
-    static udata_t step_counter;
+    // sender incoming data cache
+    static const uint16_t* data_cur_pointer = 0;
+    // sender state machine
+    static uint32_t step_counter;
+    static uint8_t  data_step_nr;
+    static uint8_t  data_digit;
 
-    static uint8_t data_this = SEQ_NONE;
-    static uint8_t data_step;
-    static uint8_t data_on;
-
-    if ( loop_counter!=0 ) {
-        loop_counter--;
+    if ( 0 == data_cur_pointer ) {
+        if ( 0 != data_next_pointer ) {
+            // accepting the next sequence
+            data_cur_pointer = data_next_pointer;
+            data_next_pointer = 0;
+            // starting a new sequence
+            data_step_nr = 0;
+            step_counter = STEP_SIZE * (uint32_t)data_cur_pointer[data_step_nr];
+            data_digit = 1;
+            sendON();
+            sendStartONE();
+        } else {
+            // disable the timer interrupt
+            // so that it does not continue until enabled again
+            sendOFF();
+            T1intOFF();
+        }
     } else {
-        loop_counter = SEQ_STEP;
-
-        if ( SEQ_NONE == data_this ) {
-            if ( SEQ_NONE != data_next ) {
-                data_this = data_next;
-                data_next = SEQ_NONE;
-                data_step = 0;
-                step_counter = ARRAY[data_this][data_step];
-                step_counter--;
-                data_on = 1;
+        if ( 0 < step_counter) {
+            step_counter--;
+            if (data_digit) sendGoOnONE(); else sendGoOnTWO(); // keep transmitting
+        } else {
+            data_step_nr++;
+            if ( 0 != data_cur_pointer[data_step_nr] ) {
+                step_counter = STEP_SIZE * (uint32_t)data_cur_pointer[data_step_nr];
+                data_digit = ! data_digit;
+                if (data_digit) sendStartONE(); else sendStartTWO(); // keep transmitting
             } else {
-                data_on = 0;
-                // disable the timer interrupt
-                // so that it does not continue
-                // until enabled again
-                T1intOFF();
-            }
-            } else {
-            if ( 0 < step_counter) {
-                step_counter--;
-                } else {
-                data_step++;
-                if ( 0 != ARRAY[data_this][data_step] ) {
-                    data_on = ! data_on;
-                    step_counter = ARRAY[data_this][data_step];
-                    step_counter--;
-                } else {
-                    data_on = 0;
-                    data_this = SEQ_NONE;
-                }
+                data_digit = 0;
+                data_cur_pointer = 0;
+                sendOFF();
             }
         }
     }
 
-    if (data_on) sendON();
-    else sendOFF();
 }
 
 
 
 int main(void)
 {
+    const static uint16_t morseA[] = { 1, 1, 3, 7, 0 };
+    const static uint16_t morseO[] = { 3, 1, 3, 1, 3, 7, 0 };
+    const static uint16_t morseSOS[] = { 1, 1, 1, 1, 1, 4, 3, 1, 3, 1, 3, 4, 1, 1, 1, 1, 1, 7, 0 };
+
     leds_init();
     leds_set(0);
     keys_init();
@@ -110,14 +118,11 @@ int main(void)
 
     sei();
 
-    uint8_t but_prev = 0;
     while(1) {
         uint8_t but_cur = keys_get();
-        uint8_t but_chg = ( but_cur^but_prev ) & but_cur;
-        but_prev = but_cur; // important! update what is now current will be past next time
-
-        if ( (but_chg & B_K4) !=0 ) send(0);
-        if ( (but_chg & B_K5) !=0 ) send(1);
+        if ( (but_cur & B_K4) !=0 )      sendMessage(morseA);
+        else if ( (but_cur & B_K5) !=0 ) sendMessage(morseO);
+        else if ( (but_cur & B_K6) !=0 ) sendMessage(morseSOS);
     }
 
     return(0);
